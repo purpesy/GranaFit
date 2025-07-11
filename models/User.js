39d4b2a82e00e1ccf1f49b82476685e810d04a09
purpesy <lucasgabrieldossantos753@gmail.com>
2025-07-11 19:13:27 -0300

    Fiz as rotas para buscar todos users e buscar user por ID

---
+++ models/User.js
@@ -0,0 +1,45 @@
+var db = require("../database/connection.js");
+
+class User {
+  async findAll() {
+    try {
+      var result = await db.select().from("tbl_users");
+      return result;
+    } catch (error) {
+      console.error("Erro ao buscar usuários: ", error);
+      throw error;
+    }
+  }
+
+  async findById(id) {
+    try {
+      var result = await db.select().from("tbl_users").where("id_user", id);
+      return result;
+    } catch (error) {
+      console.error("Erro ao buscar usuário: ", error);
+      throw error;
+    }
+  }
+
+  async create(userData) {
+    try {
+      const result = await db("tbl_users").insert({
+        nome_user: userData.nome_user,
+        foto_user: userData.foto_user || null,
+        email_user: userData.email_user,
+        senha_user: userData.senha_user,
+        plano_user: userData.plano_user || "free",
+        limite_transacoes: userData.limite_transacoes || 100,
+        token_verificacao: userData.token_verificacao || null,
+        email_verificado: false,
+        status_user: "ativo",
+      });
+      return result;
+    } catch (error) {
+      console.error("Erro ao criar usuário:", error);
+      throw error;
+    }
+  }
+}
+
+module.exports = new User();
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,10 @@
 var express =  require('express');
 var HomeController = require("../controllers/HomeController")
+var UserController = require("../controllers/UserController")
 
 var router = express.Router();
 router.get("/", HomeController.index);
+router.get("/users", UserController.index);
+router.get("/users/:id", UserController.findUser);
 
 module.exports = router;
